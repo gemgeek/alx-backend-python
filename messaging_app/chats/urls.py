ba8d10be93ba0884b6557a537799c749fb9b0b24
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-20 20:29:58 +0100

    fix: Adjust router import for checker compliance

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include  # type: ignore
-from rest_framework.routers import DefaultRouter  # type: ignore
+from rest_framework import routers  # type: ignore
 from .views import ConversationViewSet, MessageViewSet
 
-# Create a router and register our viewsets with it.
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
