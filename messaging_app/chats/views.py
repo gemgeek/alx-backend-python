5df5ec684b66d7768047f7b97cfecbd362ea2080
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-20 20:26:06 +0100

    feat: Add API URL routing and view enhancements

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,46 +1,40 @@
-from rest_framework import viewsets, permissions  # type: ignore
+from rest_framework import viewsets, permissions, status  # type: ignore
+from rest_framework.response import Response  # type: ignore
+from django_filters.rest_framework import DjangoFilterBackend  # type: ignore
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
-
 class ConversationViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows conversations to be viewed or edited.
-    """
-    
     serializer_class = ConversationSerializer
-    
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        """
-        This view should return a list of all the conversations
-        for the currently authenticated user.
-        """
-    
         return self.request.user.conversations.all()
+
+    def create(self, request, *args, **kwargs):
+        """
+        Override create to handle conversation creation more explicitly
+        and return a 201 CREATED status.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows messages to be viewed or created.
-    """
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['conversation'] # Allows filtering messages by conversation ID
+
     def get_queryset(self):
-        """
-        This view should return a list of all messages in conversations
-        that the current user is a part of.
-        """
-        
         user_conversations = self.request.user.conversations.all()
         return Message.objects.filter(conversation__in=user_conversations)
 
     def perform_create(self, serializer):
-        """
-        Automatically set the sender of a new message to the current user.
-        """
-        
         serializer.save(sender=self.request.user)
