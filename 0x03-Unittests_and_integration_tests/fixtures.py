d545640e1cf322b94d26d69aad8be3cca8ec2c19
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-20 08:50:50 +0100

    Update fixtures.py

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+"""
+Fixtures for GithubOrgClient tests
+"""
+
+org_payload = {
+    "login": "google",
+    "id": 1,
+    "repos_url": "https://api.github.com/orgs/google/repos",
+}
+
+repos_payload = [
+    {"id": 1, "name": "repo1", "license": {"key": "apache-2.0"}},
+    {"id": 2, "name": "repo2", "license": {"key": "other"}},
+]
+
+expected_repos = ["repo1", "repo2"]
+
+apache2_repos = ["repo1"]
 
 TEST_PAYLOAD = [
   (
