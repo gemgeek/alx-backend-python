0ea3c7b54e258aa036ad1a49a81a05370f478daa
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-22 16:12:17 +0100

    feat: add basic request logging middleware

---
+++ Django-Middleware-0x03/chats/middleware.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        user = request.user if request.user.is_authenticated else "Anonymous"
+        log_message = f"[{datetime.now()}] User: {user} - Path: {request.path}\n"
+
+        # Log the message to a file
+        with open("requests.log", "a") as log_file:
+            log_file.write(log_message)
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
