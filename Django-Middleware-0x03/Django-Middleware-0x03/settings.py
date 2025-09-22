baac8a5b321705f3dcb142783bbc10c23f95bbb5
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-22 16:29:54 +0100

    refactor: align settings directory with project name

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = 'Django-Middleware-0x03.urls'
 
 TEMPLATES = [
     {
