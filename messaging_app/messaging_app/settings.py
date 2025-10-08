28936430e1f99a72397c3a845744439b7e18705b
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-10-08 19:21:29 +0100

    feat: implement multi-container setup with Docker Compose

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DB'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'HOST': 'db', # This is the service name from docker-compose.yml
+        'PORT': 3306,
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
     }
 }
 
